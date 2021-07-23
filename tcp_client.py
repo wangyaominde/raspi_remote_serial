f5764e02a6b9e96853125dd6463062c94149e0a9
wangyaominde <wangyaominde@qq.com>
2021-07-23 14:57:58 +0800

    create udp c/s

--- tcp_client.py
+++ tcp_client.py
@@ -1,11 +1,11 @@
 import socket
 
-def connect_tcp_server(port):
+def connect_tcp_server(host,port):
     """connect tcp server use port"""
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("127.0.0.1", port))
+    sock.connect((host, port))
     sock.send("Hello, I am a client".encode("utf-8"))
     print(sock.recv(1024).decode("utf-8"))
     sock.close()
 
-connect_tcp_server(8888)
+connect_tcp_server("192.168.0.195",8888)
