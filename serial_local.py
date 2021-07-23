64f30f3fdb02040a8ccdde49f2bac00544245640
wangyaominde <wangyaominde@qq.com>
2021-07-23 14:51:05 +0800

    修改

--- serial_local.py
+++ serial_local.py
@@ -5,8 +5,8 @@ def get_all_serial_port():
     获取当前系统所有可用串口
     :return:
     """
-    port_list = []
-    port_list = serial.tools.list_ports.comports()
-    return port_list
+    import serial.tools.list_ports
+    return [port[0] for port in serial.tools.list_ports.comports()]
+
 
 print(get_all_serial_port())
