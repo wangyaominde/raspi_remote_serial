f91ad65439430a7e4bab846ee7c6467fd10cf935
wangyaominde <wangyaominde@qq.com>
2021-07-23 14:46:33 +0800

    测试串口获取文件

--- serial_local.py
+++ serial_local.py
@@ -1,8 +1,13 @@
 import serial
 
-class ser():
-    def get_all_serial_port():
-        """获取系统所有串口号"""
-        import glob
-        return glob.glob('/dev/ttyUSB*') + glob.glob('/dev/ttyACM*') + glob.glob('/dev/ttyAMA*')
+def get_all_serial_port():
+    """
+    获取当前系统所有可用串口
+    :return:
+    """
+    port_list = []
+    for port in list(serial.tools.list_ports.comports()):
+        port_list.append(port[0])
+    return port_list
 
+print(get_all_serial_port())
