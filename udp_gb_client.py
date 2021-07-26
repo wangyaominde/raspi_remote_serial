2dbd62eac1d1efac80962c5448d32c0adb355f72
wangyaominde <wangyaominde@qq.com>
2021-07-26 23:11:01 +0800

    调整了广播udp的返回值

--- udp_gb_client.py
+++ udp_gb_client.py
@@ -11,15 +11,19 @@ PORT = 1060
 s.bind(('', PORT))
 print('Listening for broadcast at ', s.getsockname())
 
-while True:
-    data, address = s.recvfrom(65535)
-    if data.startswith(b'raspi_remote_serial use port = '):
-        print('Server: ', address, data)
-        tcp_ser_addr=address
-        tcp_ser_port=data[-4:]
-    break
 
-tcp_ser_addr=tcp_ser_addr[0]
-tcp_ser_port=int(tcp_ser_port)
-print('tcp_ser_addr=',tcp_ser_addr)
-print('tcp_ser_port=',tcp_ser_port)
+def get_tcp_port_address():
+    while True:
+        data, address = s.recvfrom(65535)
+        if data.startswith(b'raspi_remote_serial use port = '):
+            print('Server: ', address, data)
+            tcp_ser_addr=address
+            tcp_ser_port=data[-4:]
+        break
+
+    tcp_ser_addr=tcp_ser_addr[0]
+    tcp_ser_port=int(tcp_ser_port)
+    #print('tcp_ser_addr=',tcp_ser_addr)
+    #print('tcp_ser_port=',tcp_ser_port)
+    return tcp_ser_addr,tcp_ser_port
+
