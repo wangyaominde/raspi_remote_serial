9526c67b91b29271164ecf72aed1aa50a83cddc1
wangyaominde <wangyaominde@qq.com>
2021-07-26 23:58:21 +0800

    添加延迟

--- tcp_client.py
+++ tcp_client.py
@@ -1,11 +1,14 @@
 import socket
+import time
 
 def connect_tcp_server(host,port):
     """connect tcp server use port"""
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
-    sock.send("Hello, I am a client".encode("utf-8"))
-    print(sock.recv(1024).decode("utf-8"))
-    sock.close()
+    while True:
+        print(host,port)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        sock.send("Hello, I am a client".encode("utf-8"))
+        print(sock.recv(1024).decode("utf-8"))
+        sock.close()
+        time.sleep(3)
 
-connect_tcp_server("192.168.0.195",8888)
