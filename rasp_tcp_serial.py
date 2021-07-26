c752e3d83110411920c7f56fd95fedafa5f3246a
wangyaominde <wangyaominde@qq.com>
2021-07-26 23:48:33 +0800

    添加了连接方式

--- rasp_tcp_serial.py
+++ rasp_tcp_serial.py
@@ -1,6 +1,9 @@
 import serial_local
 import udp_gb_client
+import tcp_client
 
 tcp_addr,tcp_port = udp_gb_client.get_tcp_port_address()
 print("tcp_addr:",tcp_addr)
 print("tcp_port:",tcp_port)
+
+tcp_client.connect_tcp_server(tcp_addr,tcp_port)
