e517be7392d5596f509bb1351ad44510d9889d45
wangyaominde <wangyaominde@qq.com>
2021-07-27 00:37:21 +0800

    fix bug

--- rasp_tcp_serial.py
+++ rasp_tcp_serial.py
@@ -1,6 +1,6 @@
 import serial_local
 import udp_gb_client
 
-udp_addr, udp_port = udp_gb_client.get_udp_info()
+udp_addr, udp_port = udp_gb_client.get_trans_port_address()
 
 print("UDP: %s:%d" % (udp_addr, udp_port))
