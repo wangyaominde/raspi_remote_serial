b4b7a57d1c473e28d53df60e20b246bc01dffe91
wangyaominde <wangyaominde@qq.com>
2021-07-27 01:12:13 +0800

    使用json传送ser_list

--- rasp_tcp_serial.py
+++ rasp_tcp_serial.py
@@ -1,9 +1,10 @@
 import serial_local
 import udp_raspberry_client
+import json
 
 udp_addr, udp_port = udp_raspberry_client.get_trans_port_address()
 
 print("UDP: %s:%d" % (udp_addr, udp_port))
 ser_list=serial_local.get_all_serial_port()
-print(ser_list)
-udp_raspberry_client.send_data(udp_ser_address=udp_addr,udp_ser_port=udp_port,data=ser_list.to_bytes(2,byteorder='big'))
+print(json.dumps(ser_list))
+udp_raspberry_client.send_data(udp_ser_address=udp_addr,udp_ser_port=udp_port,data=json.dumps(ser_list))
