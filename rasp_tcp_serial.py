8f9c2d06ffd57962eed4deedecfab9d3ad1eb475
wangyaominde <wangyaominde@qq.com>
2021-07-27 00:12:34 +0800

    调整

--- rasp_tcp_serial.py
+++ rasp_tcp_serial.py
@@ -6,4 +6,4 @@ tcp_addr,tcp_port = udp_gb_client.get_tcp_port_address()
 print("tcp_addr:",tcp_addr)
 print("tcp_port:",tcp_port)
 
-tcp_client.connect_tcp_server(tcp_addr,tcp_port)
+tcp_client.connect_tcp_server(tcp_addr,int(tcp_port))
