5ebb900b0b415b0a09b360ff2c32c1b44cac4a5e
wangyaominde <wangyaominde@qq.com>
2021-08-12 16:21:39 +0800

    update

--- rasp_udp_serial.py
+++ rasp_udp_serial.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import serial_local
-import mqtt_client
-import json
-import threading
 
-
-
-threading.Thread(target=mqtt_client.mqtt_sub("serlist")).start()
+ser_port_list = serial_local.get_all_serial_port()
+print(ser_port_list)
