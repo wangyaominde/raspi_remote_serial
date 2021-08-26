1ad4322505687e9b42bf18f220ebe7736383cd86
wangyaominde <wangyaominde@qq.com>
2021-08-27 01:01:54 +0800

    重新规划

--- rasp_mqtt_serial.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import mqtt_client_send
-import serial_local
-import json
-
-ser_port_list = serial_local.get_all_serial_port()
-mqtt_client_send.mqtt_client_send("serial_port_number", json.dumps(ser_port_list))
-print(ser_port_list)
